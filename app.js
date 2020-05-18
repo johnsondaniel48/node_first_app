00b1448c2230087ed15abd0c27688c90345ae911
Johnson Daniel <j.daniel@acrrm.org.au>
2020-05-18 16:41:57 +1000

    test
    test

---
+++ app.js
@@ -0,0 +1,39 @@
+const logger = require("./logger");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+
+//module export
+// logger("hello");
+
+// Built in Path Module
+// var pathObj = path.parse(__filename);
+// console.log(pathObj);
+
+// Built in OS Module
+// var freeMemory = os.freemem();
+// var totalMemory = os.totalmem();
+// var hostName = os.hostname();
+// var platform = os.platform();
+// var uptime = os.uptime();
+// var release = os.release();
+// console.log(`free memory is : ${freeMemory}`);
+// console.log(`total memory is : ${totalMemory}`);
+// console.log(`the hostname is :${hostName}`);
+// console.log(`the platform is :${platform}`);
+// console.log(`uptime is:${uptime}`);
+// console.log(`release : ${release}`);
+
+// Built in FileSystem Module
+// const files = fs.readdirSync("./");
+// console.log(files);
+
+// fs.readdir("./", function(err, files) {
+//   if (err) console.log(`error is :${err}`);
+//   else console.log(`result is : ${files}`);
+// });
+
+// fs.readdir("$", function(err, files) {
+//   if (err) console.log(`error is :${err}`);
+//   else console.log(`result is : ${files}`);
+// });
